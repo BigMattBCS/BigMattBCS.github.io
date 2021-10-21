d1aef9eb3e73135a15db8a89c5cd4e12de22fcd6
BigMattBCS <92554635+BigMattBCS@users.noreply.github.com>
2021-10-20 22:12:44 -0400

    Update and rename logIn.html to logIn.php

--- logIn.php
+++ logIn.php
@@ -1,3 +1,47 @@
+<?php
+
+session_start();
+
+include("connection.php");
+include("functions.php");
+
+if($_SERVER['REQUEST_METHOD'] == "POST")
+{
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    if(!empty($username) && !empty($password))
+    {
+       
+        $query = "select * from account where username = '$username' limit 1";
+
+        $result = mysqli_query($con, $query);
+
+        if($result)
+        {
+            if($result && mysqli_num_rows($result) > 0)
+            {
+                $user_data = mysqli_fetch_assoc($result);
+                
+                if($user_data['password'] == $password)
+                {
+                    $_SESSION['user_id'] = $user_data['user_id'];
+                    header("Location: index.php");
+                    die;
+                }
+            }
+        }
+        echo "wrong username or password";
+    }
+    else
+    {
+        echo "Please enter some valid information";
+    }
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html>
 <head>
